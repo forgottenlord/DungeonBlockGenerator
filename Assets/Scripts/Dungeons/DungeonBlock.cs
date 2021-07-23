565a13d653eb2a61127c699430ae5ab93b976af4
forgotten_lord <forgotten_lord4@mail.ru>
2021-07-23 16:18:43 +0300

    поправил алгоритм добавил автоподгон по размеру блока

---
+++ Assets/Scripts/Dungeons/DungeonBlock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonAssault
+{
+    public class DungeonBlock
+    {
+        public DungeonBlockType t;
+        public DungeonBlockRotation r;
+        public int x;
+        public int y;
+        public DungeonBlockNeighbors code;
+    }
+}
---
+++ Assets/Scripts/Dungeons/DungeonBlockNeighbors.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonAssault
+{
+    [System.Flags]
+    public enum DungeonBlockNeighbors : byte
+    {
+        Left = 1,
+        Back = 2,
+        Right = 4,
+        Front = 8,
+    }
+}
